baecde7a8282b8af5e87812c7afdc61da169f1dc
Jari Voutilainen <jari.voutilainen@iki.fi>
2011-07-20 13:33:58 +0300

    Support for NPOT canvas textures

--- src/material/glge_texturecanvas.js
+++ src/material/glge_texturecanvas.js
@@ -127,9 +127,15 @@ GLGE.TextureCanvas.prototype.doTexture=function(gl){
 	if(!this.glTexture){
 		this.glTexture=gl.createTexture();
 		gl.bindTexture(gl.TEXTURE_2D, this.glTexture);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 		this.updateCanvas(gl);
 	}else{
 		gl.bindTexture(gl.TEXTURE_2D, this.glTexture);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 		if(this.autoUpdate || this.doUpdate) this.updateCanvas(gl);
 	}
 	this.doUpdate=false;
